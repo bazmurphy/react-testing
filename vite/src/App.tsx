ce831d426a4a446286e642be91505c897ab1761a
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-08-26 15:01:59 +0100

    adjustments

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,19 +1,44 @@
 import { Page } from "./Page";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const start = 3;
-  const end = 19;
+  const pageStart = 3;
+  const pageEnd = 19;
   const pageNumbers = Array.from(
-    { length: end - start + 1 },
-    (_, index) => index + start
+    { length: pageEnd - pageStart + 1 },
+    (_, index) => index + pageStart
   );
-  console.log(pageNumbers);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + pageNumbers.length) % pageNumbers.length
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % pageNumbers.length);
+  };
+
   return (
     <div className="App">
-      {pageNumbers.map((pageNumber) => (
-        <Page key={pageNumber} pageNumber={pageNumber} />
-      ))}
+      <nav>
+        <button onClick={handlePrevious}>
+          <span className="arrow">&lt;</span>
+          <span className="page-number">
+            {((currentIndex - 1 + pageNumbers.length) % pageNumbers.length) + 3}
+          </span>
+        </button>
+        <button onClick={handleNext}>
+          <span className="page-number">
+            {((currentIndex + 1) % pageNumbers.length) + 3}
+          </span>
+          <span className="arrow">&gt;</span>
+        </button>
+      </nav>
+      <Page key={currentIndex} pageNumber={currentIndex + 3} />
     </div>
   );
 }
